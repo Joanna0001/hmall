3565bc113afe65a75f867575ace49ecc4c89f637
Joanna0001 <gkj159@163.com>
2025-09-28 12:06:38 +0800

    chore: seata

--- item-service/src/main/java/com/heima/item/controller/ItemController.java
+++ item-service/src/main/java/com/heima/item/controller/ItemController.java
@@ -43,7 +43,7 @@ public class ItemController {
     @ApiOperation("根据id查询商品")
     @GetMapping("{id}")
     public ItemDTO queryItemById(@PathVariable("id") Long id) {
-        ThreadUtil.sleep(500);  // 模拟业务延迟
+        // ThreadUtil.sleep(500);  // 模拟业务延迟
         return BeanUtils.copyBean(itemService.getById(id), ItemDTO.class);
     }
 
